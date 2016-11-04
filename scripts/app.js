325f76b20f0d5dfd8bb1b1496c5d5297959ea4fc
hayden7913 <hayden321@gmail.com>
2016-11-04 15:27:56 -0700

    renders word translations

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,53 @@
-var BASE_URL = "https://glosbe.com/gapi/translate";
 
-function getApiData(searchString, callback){
+
+function getApiData(BASE_URL, searchString, elements, callback){
+	var elementObj = elements;
+	var term = searchString;
 	var query = {
 		from: "eng",
 		dest: "spa",
 		format: "json",
-		phrase: "apple",
+		phrase: searchString,
 		pretty: true
-
 	}
-	$.getJSON(BASE_URL, query, callback);
+
+	$.getJSON(BASE_URL, query, callback(term, elementObj));
 }
 
-function processSearchResults(data) {
-	var searchResults = {
-		translation: data.tuc[0].phrase.text,
-		nativeDef: data.tuc[0].meanings[2].text,
-		targetDef: data.tuc[0].meanings[1].text,
+function renderSearchResults(term, elements) {
+	return function(data) {
+		console.log(term);	
+		var translation = data.tuc[0].phrase.text;
+		var nativeDef = data.tuc[0].meanings[2].text;
+		var targetDef = data.tuc[0].meanings[1].text;
+		elements.term.text(term);
+		elements.trans.text(translation);	
+		elements.nativeDef.text(nativeDef);	
+		elements.targetDef.text(targetDef);	
 	}
-		
-	//renderSearchResults(searchResults);
 }
 
-function submitHandler(state) {
+function submitHandler(BASE_URL, elements) {
 	$("form").submit(function(e){
 		e.preventDefault();
 		var searchString = $("input[name='js-vidSearch']").val();
-		state.term = searchString;
-		getApiData(searchString, processSearchResults);
+		getApiData(BASE_URL, searchString, elements, renderSearchResults);
 	});	
 }
 
+function main() {
+	var BASE_URL = "https://glosbe.com/gapi/translate";
+	var elements = {
+		results: $(".js-results"),
+		termTrans: $(".js-termTrans"),
+		trans: $(".js-trans"),
+		term: $(".js-term"),
+		nativeDef: $(".js-nativeDef"),
+		targetDef: $(".js-targetDef")
+	}
+	submitHandler (BASE_URL, elements);
+	//elements.term.text("hello");
+}
 
 
-$(submitHandler());
+$(main());
