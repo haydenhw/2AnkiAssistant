c8f3cf474effe2e9ab5b4a8aa10bed0e2242c8a0
hayden7913 <hayden321@gmail.com>
2016-11-07 09:11:06 -0800

    fixed repeat bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,25 @@
+var state = {
+	currTerm: null,
+	wordList: []
+}
 
+function processSearchResults(state, term, elements) {
+	return function(data) {
+		var termData = {
+			term: term, 
+ 			translation: data.tuc[0].phrase.text,
+ 			nativeDef: data.tuc[0].meanings[2].text,
+ 			targetDef: data.tuc[0].meanings[1].text,
+	 	}
+	state.currTerm = termData; 	
+	renderSearchResults(termData, elements);
+	}
+}
 
-function getApiData(BASE_URL, searchString, elements, callback){
+function getApiData(state, BASE_URL, searchString, callback, elements){
 	var elementObj = elements;
 	var term = searchString;
+	var state = state;
 	var query = {
 		from: "eng",
 		dest: "spa",
@@ -11,28 +28,54 @@ function getApiData(BASE_URL, searchString, elements, callback){
 		pretty: true
 	}
 
-	$.getJSON(BASE_URL, query, callback(term, elementObj));
+	$.getJSON(BASE_URL, query, callback(state, term, elementObj));
 }
 
-function renderSearchResults(term, elements) {
-	return function(data) {
-		console.log(term);	
-		var translation = data.tuc[0].phrase.text;
-		var nativeDef = data.tuc[0].meanings[2].text;
-		var targetDef = data.tuc[0].meanings[1].text;
-		elements.term.text(term);
-		elements.trans.text(translation);	
-		elements.nativeDef.text(nativeDef);	
-		elements.targetDef.text(targetDef);	
-	}
+
+
+function renderSearchResults(termData, elements){
+		elements.term.text(termData.term);
+		elements.trans.text(termData.translation);	
+		elements.nativeDef.text(termData.nativeDef);	
+		elements.targetDef.text(termData.targetDef);	
 }
 
-function submitHandler(BASE_URL, elements) {
+
+function renderList(state){
+
+
+	var listHTML = state.wordList.map(term =>
+			"<div class='listItem'>"+
+			"	<div class='term inline'>" + term.term +"</div>"+
+			"	<div class='trans inline'>" + term.translation + "</div>"+
+			"<button class='inline' name='removeTerm'>Remove</button>"+
+			"</div>"
+		);
+
+	$(".js-list").html(listHTML);
+}
+
+function submitHandler(state, BASE_URL, elements) {
 	$("form").submit(function(e){
 		e.preventDefault();
 		var searchString = $("input[name='js-vidSearch']").val();
-		getApiData(BASE_URL, searchString, elements, renderSearchResults);
+		getApiData(state, BASE_URL, searchString, processSearchResults, elements);
+		//renderSearchResults(elements, termData)
 	});	
+}
+
+function addTermHandler(state) {
+	$(".js-addTerm").click(function(){
+		state.wordList.push(state.currTerm);
+		renderList(state);
+	});
+}
+
+function removeTermHandler(state){
+	$(".js-list").on("click", "button" ,function(){
+		console.log("hello");
+		//removeTerm(state);
+	});
 }
 
 function main() {
@@ -45,8 +88,10 @@ function main() {
 		nativeDef: $(".js-nativeDef"),
 		targetDef: $(".js-targetDef")
 	}
-	submitHandler (BASE_URL, elements);
-	//elements.term.text("hello");
+	submitHandler (state, BASE_URL, elements);
+	addTermHandler(state);
+	removeTermHandler(state);	
+
 }
 
 
